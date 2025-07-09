c47ed95af0c7572de8a7277c3334d269108a6d08
waynelai <waynelai06@gmail.com>
2025-07-09 15:16:18 +0800

    feat: log viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,12 @@ class AppServiceProvider extends ServiceProvider
             resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
             $this->app['request']->server->set('HTTPS', true);
         }
-        
+
         Vite::prefetch(concurrency: 3);
+
+        LogViewer::auth(function ($request) {
+            return $request->user()
+                && $request->user()->email == 'test@test.com';
+        });
     }
 }
